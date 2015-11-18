a22551f6e453f5ad45cc6b1b2adc54be99553941
Linzai1994 <842875208@qq.com>
2015-11-18 18:06:37 +0800

    update

--- src/com/bearhunter/DotinPolygon.java
+++ src/com/bearhunter/DotinPolygon.java
@@ -6,30 +6,38 @@ package com.bearhunter;
 public class DotinPolygon {
 
 	public static void main(String[] args) {
-		Dot testdot = new Dot(0.36, 0.45);
-		Dot[] Dots = { new Dot(0, 0), new Dot(0, 2), new Dot(2, 2),
-				new Dot(0, 2) };
+		Dot testdot = new Dot(1.9, 1.9);
+		Dot[] Dots = { new Dot(0, 0), new Dot(0, 2),/* new Dot(0.1, 2.1),
+				new Dot(0.2, 2.2), new Dot(0.3, 2.3), new Dot(0.4, 2.4),
+				new Dot(0.5, 2.5), new Dot(0.6, 2.6), new Dot(0.7, 2.7),
+				new Dot(0.8, 2.8), new Dot(0.9, 2.9), */new Dot(2, 2),
+				new Dot(4, 0) };
+		long beforetime = System.currentTimeMillis();
+		for (int i = 0; i < 100000; i++) {
 
-		System.out.println(new DotinPolygon().pointInPolygon(Dots, testdot));
+			System.out
+					.println(new DotinPolygon().pointInPolygon(Dots, testdot));
+		}
+		long aftertime = System.currentTimeMillis();
+		System.out.println("算法耗时 ：" + (aftertime - beforetime) + "毫秒。");
 	}
 
 	/**
 	 * 射线法
+	 * 
 	 * @param Dots
 	 * @param testdot
 	 * @return
 	 */
-	public boolean pointInPolygon(Dot[] Dots, Dot testdot) {
+	public String pointInPolygon(Dot[] Dots, Dot testdot) {
 
 		int i, j = Dots.length - 1;
 		boolean oddNodes = false;
 
 		for (i = 0; i < Dots.length; i++) {
 			if ((Dots[i].getX() < testdot.getX()
-					&& Dots[j].getY() >= testdot.getY() || Dots[j].getY() < testdot
-					.getY() && Dots[i].getY() >= testdot.getY())
-					&& (Dots[i].getX() <= testdot.getX() || Dots[j].getX() <= testdot
-							.getX())) {
+					&& Dots[j].getY() >= testdot.getY() || Dots[j].getY() < testdot.getY() && Dots[i].getY() >= testdot.getY())
+					&& (Dots[i].getX() <= testdot.getX() || Dots[j].getX() <= testdot.getX())) {
 				oddNodes ^= (Dots[i].getX() + (testdot.getY() - Dots[i].getY())
 						/ (Dots[j].getY() - Dots[i].getY())
 						* (Dots[j].getX() - Dots[i].getX()) < testdot.getX());
@@ -37,7 +45,7 @@ public class DotinPolygon {
 			j = i;
 		}
 
-		return oddNodes;
+		return oddNodes ? "该点在内部" : "该点不在内部";
 	}
 }
 
